7f91d626e2485006c8b06949a59c0e4739206142
Hudson Lanier <Hudsonlanier@gmail.com>
2022-08-04 14:49:00 -0400

    have all pages and navigation staged with dummy entries

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -7,6 +7,7 @@ const Home = () => {
             
             <div>
             <h1 className='main-text'>Ten Squid Wins</h1>
+            <h5 className='sub-text typewriter'>A Frontend Playground and Portfolio for Dr. Hudson Lanier </h5>
                 <div className="snow_wrap">
 	                <div className="snow"></div>
                 </div>
---
+++ src/components/playground/index.js
@@ -0,0 +1,13 @@
+import './index.scss';
+
+const Playground = () => {
+    return (
+        <div className='container playground-page'>
+            <div className='text-zone'>
+                <h1 className='animated-bg-text'>FRONTEND</h1>
+                <h1 className='animated-bg-text'>PLAYGROUND</h1>
+            </div>
+        </div>
+    )
+}
+export default Playground
