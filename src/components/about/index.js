c1b4fb948e83cec022a4fa1698b476ed6cafa4fd
Hudson Lanier <Hudsonlanier@gmail.com>
2022-08-09 15:43:09 -0400

    made a bunch of scss adjustments

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -3,11 +3,14 @@ import './index.scss';
 const About = () => {
     return (
         
-        <div className='container about-page'>   
+        <div className='about-page'>   
             <div className='text-zone'>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> <br/> 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> <br/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> <br/>
+                <h1 className='about-heading'>What would you like to know about?</h1> <br />
+                <div className='options'>
+                    <button className='option'>Frontend Skills</button>
+                    <button className='option'>Backend Skills</button>
+                    <button className='option'>Dr. Hudson Lanier</button>
+                </div>
             </div>
         
         </div>
