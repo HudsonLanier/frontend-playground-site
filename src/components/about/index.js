9465a14f216e2e65cff4f7bb1f03505386ae50b6
Hudson Lanier <Hudsonlanier@gmail.com>
2022-08-22 12:45:38 -0400

    changes to home and menu design

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,19 +1,46 @@
 import './index.scss';
+const some_button_text = ['Frontend Skills', 'Backend Skills', 'About Me'];
+const frontend_skills = ['Comprehensive List', 'This Site'];
 
-const About = () => {
-    return (
-        
-        <div className='about-page'>   
-            <div className='text-zone'>
-                <h1 className='about-heading'>What would you like to know about?</h1> <br />
-                <div className='options'>
-                    <button className='option'>Frontend Skills</button>
-                    <button className='option'>Backend Skills</button>
-                    <button className='option'>Dr. Hudson Lanier</button>
-                </div>
-            </div>
-        
-        </div>
-    )
+
+function BTNMaker(props) {
+    return <button>{props.text}</button>;
+  }
+
+function MakeButtons(props) {
+    let buttons = [];
+    for (let i = 0; i < props.array.length; i++) {
+        let newest_button = <BTNMaker text= {props.array[i]}/>;
+        buttons.push(newest_button);
+
+  }
+  console.log(buttons);
+  return buttons;
 }
-export default About
+  
+function Option() {
+    return (
+        <div className='about-page'>
+            <MakeButtons array={some_button_text} />
+        </div>
+    );
+  }
+
+// const About = () => {
+//     return (
+        
+//         <div className='about-page'>   
+//             <div className='text-zone'>
+//                 <h1 className='about-heading'>What would you like to know about?</h1> <br />
+//                 <div className='options'>
+//                     <button className='option'>Frontend Skills</button>
+//                     <button className='option'>Backend Skills</button>
+//                     <button className='option'>Dr. Hudson Lanier</button>
+//                     <Option text="Testing" />
+//                 </div>
+//             </div>
+        
+//         </div>
+//     )
+// }
+export default Option
